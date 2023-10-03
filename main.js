9eddadeee645d55d9a7211118da8f22b39eaee29
Isabela Haddad Stravolo <102175528+IsabelaHaddad@users.noreply.github.com>
2023-10-03 07:43:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ function tocaSomPom() {
   document.querySelector('som_tecla_pom').play();
 }
 document.querySelector('.tecla_pom');
+
+document.querySelector ('.tecla_pom').onclick = tocaSomPom;
+
+
