d1340e9637bac5d970f863043828a4dc51decdbd
Isabela Haddad Stravolo <102175528+IsabelaHaddad@users.noreply.github.com>
2023-10-26 16:44:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-function tocaSomPom() {
-  document.querySelector('#som_tecla_pom').play();
+
+function tocasSomPom () {
+document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelectorAll('.tecla')
+const ListaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador= 0;
 
+//enquanto
+while (contador < listaDeTeclas.length ){
+
+listaDeTeclas{0}.onclick = tocaSomPom;
+
+contador= contador + 1;
+
+console.log(contador);
+
+}
