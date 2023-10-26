2f2bd7cc082039fe7dbef7b9046790df6c4af9dd
Isabela Haddad Stravolo <102175528+IsabelaHaddad@users.noreply.github.com>
2023-10-26 17:04:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -17,3 +17,21 @@ contador= contador + 1;
 console.log(contador);
 
 }
+
+
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function (){
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;  
+}
